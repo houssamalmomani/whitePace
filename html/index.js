ea641009d5275d4cfe01f5a29938a9f332b368f3
houssam almomani <houssamalmomani1@gmail.com>
2022-11-12 21:42:13 +0300

    change some layout and add swiper

---
+++ html/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 10,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});

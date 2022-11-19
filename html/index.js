b7ed90aa92790e407edf91f0fbb2f6956f24b5fb
houssam almomani <houssamalmomani1@gmail.com>
2022-11-19 15:45:33 +0300

    simplify the code

--- html/index.js
+++ html/index.js
@@ -1,9 +1,6 @@
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 10,
-  grabCursor: true,
-
+  spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,

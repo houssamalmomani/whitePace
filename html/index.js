0a42588bea7f2d273298983bf451cc5992375bf7
houssam almomani <houssamalmomani1@gmail.com>
2022-11-20 17:51:15 +0300

    changes on swiper and navigation bar

--- html/index.js
+++ html/index.js
@@ -1,29 +1,27 @@
-var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 30,
+const swiper = new Swiper('.mySwiper', {
+  // Default parameters
+  initialSlide: 1,
+
+  centeredSlides: true,
+
+  centeredSlides: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+    },
+
+    400: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+
+    1152: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+  },
 });
-// const swiper = new Swiper('.swiper', {
-//   // Default parameters
-//   slidesPerView: 3,
-//   spaceBetween: 10,
-//   // Responsive breakpoints
-//   breakpoints: {
-//     // when window width is >= 320px
-//     250: {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//     },
-//     // when window width is >= 480px
-//     786: {
-//       slidesPerView: 1,
-//     },
-//     // when window width is >= 640px
-//     1152: {
-//       slidesPerView: 1,
-//     },
-//   },
-// });
